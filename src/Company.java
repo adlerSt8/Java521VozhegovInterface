dad305e55c6ad7d41cd6bd22b401886bfa18952e
Андрей <adlerSt@yandex.ru>
2025-04-20 08:03:23 +0300

    final

--- src/Company.java
+++ src/Company.java
@@ -8,7 +8,7 @@ public class Company {
     }
 
     public List<Employee> getTopSalaryStaff(int count) {
-        if (count < 0 || count > employees.size()) {
+        if (count <= 0 || count > employees.size()) {
             return new ArrayList<>();
         } else {
             List<Employee> sorted = new ArrayList<>(employees);
